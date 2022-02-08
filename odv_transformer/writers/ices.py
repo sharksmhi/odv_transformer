110421aa2d1052ea465e0606dc7ca56d108bbc1c
Johansson Johannes <johannes.johansson@smhi.se>
2022-02-08 17:26:32 +0100

    Update ices.py

--- odv_transformer/writers/ices.py
+++ odv_transformer/writers/ices.py
@@ -111,10 +111,11 @@ class IcesOdvWriter(WriterBase):
         """Add data table to self.data_serie."""
         mapper = {c: self.pmap[c]['label']
                   for c in df.columns if c in self.pmap}
+
         col_order = self.meta_spec['mandatory'].copy()
         col_order.extend(
-            [c for c in df.columns if c not in col_order
-             and c in self.pmap and c in self.ices_mapping_spec]
+            [c for c in df.columns if c not in col_order and
+             c in self.pmap and c in self.ices_mapping_spec]
         )
         df = df[col_order].rename(columns=mapper)
 
